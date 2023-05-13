f2ad976f1a0c583ce44bf586be19b08c18639f4b
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2023-05-14 01:10:39 +0700

    alfinbudiyanto-1.2

---
+++ kuliah/pertemuan1/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'kulon2023_26102006');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+
+  $result = mysqli_query($conn, $query);
+
+  // jika hasilnya hanya 1 data
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
