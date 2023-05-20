4e07bf74c1add55f827b533cb0cf67e14c800fc0
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2023-05-21 02:23:11 +0700

    alfinbudiyanto-6.8

--- kuliah/pertemuan3/functions.php
+++ kuliah/pertemuan3/functions.php
@@ -34,14 +34,11 @@ function tambah($data)
   $jurusan = htmlspecialchars($data['jurusan']);
   $gambar = htmlspecialchars($data['gambar']);
 
-  $query = "INSERT INTO mahasiswa VALUES(
-    null,
-    '$nama',
-    '$nrp',
-    '$email',
-    '$jurusan',
-    '$gambar'
-  );";
+  $query = "INSERT INTO
+              mahasiswa
+            VALUES
+            (null, '$nama', '$nrp', '$email', '$jurusan', '$gambar');
+          ";
   mysqli_query($conn, $query) or die(mysqli_error($conn));
   return mysqli_affected_rows($conn);
 }
@@ -65,12 +62,12 @@ function ubah($data)
   $gambar = htmlspecialchars($data['gambar']);
 
   $query = "UPDATE mahasiswa SET
-    nama = '$nama',
-    nrp = '$nrp',
-    email = '$email',
-    jurusan = '$jurusan',
-    gambar = '$gambar'
-  WHERE id = $id";
+              nama = '$nama',
+              nrp = '$nrp',
+              email = '$email',
+              jurusan = '$jurusan',
+              gambar = '$gambar'
+            WHERE id = $id";
   mysqli_query($conn, $query) or die(mysqli_error($conn));
   return mysqli_affected_rows($conn);
 }
@@ -80,10 +77,11 @@ function cari($keyword)
 {
   $conn = koneksi();
 
-  $query = "SELECT * FROM mahasiswa WHERE 
-    nama LIKE '%$keyword%' OR
-    nrp LIKE '%$keyword%'
-  ";
+  $query = "SELECT * FROM mahasiswa
+              WHERE 
+            nama LIKE '%$keyword%' OR
+            nrp LIKE '%$keyword%'
+          ";
 
   $result = mysqli_query($conn, $query);
 
@@ -103,16 +101,75 @@ function login($data)
   $password = htmlspecialchars($data['password']);
 
   // cek dulu username 
-  if ($user = query("SELECT * FROM user WHERE username = '$username' && password = '$password'")) {
-    // set session
-    $_SESSION['login'] = true;
+  if ($user = query("SELECT * FROM user WHERE username = '$username'")) {
+    // cek password
+    if (password_verify($password, $user['password'])) {
+      // set session
+      $_SESSION['login'] = true;
 
-    header("Location: index.php");
-    exit;
-  } else {
-    return [
-      'error' => true,
-      'pesan' => 'Username / Password Salah!'
-    ];
+      header("Location: index.php");
+      exit;
+    }
   }
+  return [
+    'error' => true,
+    'pesan' => 'Username / Password Salah!'
+  ];
+}
+
+
+function registrasi($data)
+{
+  $conn = koneksi();
+
+  $username = htmlspecialchars(strtolower($data['username']));
+  $password1 = mysqli_real_escape_string($conn, $data['password1']);
+  $password2 = mysqli_real_escape_string($conn, $data['password2']);
+
+  // jika username / password kosong
+  if (empty($username) || empty($password2) || empty($password2)) {
+    echo "<script>
+            alert('username / password tidak boleh kosong!');
+            document.location.href = 'registrasi.php';
+          </script>";
+    return false;
+  }
+
+  // jika username sudah ada 
+  if (query("SELECT * FROM user WHERE username = '$username'")) {
+    echo "<script>
+            alert('username sudah terdaftar!');
+            document.location.href = 'registrasi.php';
+          </script>";
+    return false;
+  }
+
+  // jika konfirmasi password tidak sesuai
+  if ($password1 !== $password2) {
+    echo "<script>
+            alert('konfirmasi password tidak sesuai!');
+            document.location.href = 'registrasi.php';
+          </script>";
+    return false;
+  }
+
+  // jika password < 5 digit
+  if (strlen($password1) < 5) {
+    echo "<script>
+            alert('password terlalu pendek!');
+            document.location.href = 'registrasi.php';
+          </script>";
+    return false;
+  }
+
+  // jika username & password sudah sesuai
+  // enkripsi password
+  $password_baru = password_hash($password1, PASSWORD_DEFAULT);
+  // insert ke tabel user
+  $query = "INSERT INTO user
+              VALUES
+            (null, '$username', '$password_baru')
+          ";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
 }
