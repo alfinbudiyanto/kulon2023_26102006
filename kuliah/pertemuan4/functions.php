c0e9b0d40597ff7c40fce21513833580d45b3673
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2023-05-21 04:20:09 +0700

    alfinbudiyanto-8-fitur upload gambar

--- kuliah/pertemuan4/functions.php
+++ kuliah/pertemuan4/functions.php
@@ -24,6 +24,73 @@ function query($query)
   return $rows;
 }
 
+function upload()
+{
+  $nama_file = $_FILES['gambar']['name'];
+  $tipe_file = $_FILES['gambar']['type'];
+  $ukuran_file = $_FILES['gambar']['size'];
+  $error = $_FILES['gambar']['error'];
+  $tmp_file = $_FILES['gambar']['tmp_name'];
+
+  // ketika tidak ada gambar yang dipilih
+  if ($error == 4) {
+    echo
+    "
+      <script>
+        alert('pilih gambar terlebih dalulu!');
+      </script>
+    ";
+    return false;
+  }
+
+  // cek ekstensi file
+  $daftar_gambar = ['jpg', 'jpeg', 'png'];
+  $ekstensi_file = explode('.', $nama_file);
+  $ekstensi_file = strtolower(end($ekstensi_file));
+  if (!in_array($ekstensi_file, $daftar_gambar)) {
+    echo
+    "
+      <script>
+        alert('yang anda pilih bukan gambar! \:\|');
+      </script>
+    ";
+    return false;
+  }
+
+  // cek type file
+  if ($tipe_file != 'image/jpeg' && $tipe_file != 'image/png') {
+    echo
+    "
+      <script>
+        alert('yang anda pilih bukan gambar! \:\(');
+      </script>
+    ";
+    return false;
+  }
+
+  // cek ukuran file
+  // maksimal 5Mb == 5000000
+  if ($ukuran_file > 5000000) {
+    echo
+    "
+      <script>
+        alert('ukuran gambar terlalu besar!');
+      </script>
+    ";
+    return false;
+  }
+
+  // lolos pengecekan
+  // siap upload file
+  // generate nama file baru
+  $nama_file_baru = uniqid();
+  $nama_file_baru .= '.';
+  $nama_file_baru .= $ekstensi_file;
+  move_uploaded_file($tmp_file, 'img/' . $nama_file_baru);
+
+  return $nama_file_baru;
+}
+
 function tambah($data)
 {
   $conn = koneksi();
@@ -32,13 +99,22 @@ function tambah($data)
   $nrp = htmlspecialchars($data['nrp']);
   $email = htmlspecialchars($data['email']);
   $jurusan = htmlspecialchars($data['jurusan']);
-  $gambar = htmlspecialchars($data['gambar']);
+  // $gambar = htmlspecialchars($data['gambar']);
 
-  $query = "INSERT INTO
-              mahasiswa
-            VALUES
-            (null, '$nama', '$nrp', '$email', '$jurusan', '$gambar');
-          ";
+  // upload gambar
+  $gambar = upload();
+  if (!$gambar) {
+    return false;
+  }
+
+  $query = "INSERT INTO mahasiswa VALUES(
+    null, 
+    '$nama', 
+    '$nrp', 
+    '$email', 
+    '$jurusan', 
+    '$gambar'
+  );";
   mysqli_query($conn, $query) or die(mysqli_error($conn));
   return mysqli_affected_rows($conn);
 }
@@ -62,12 +138,12 @@ function ubah($data)
   $gambar = htmlspecialchars($data['gambar']);
 
   $query = "UPDATE mahasiswa SET
-              nama = '$nama',
-              nrp = '$nrp',
-              email = '$email',
-              jurusan = '$jurusan',
-              gambar = '$gambar'
-            WHERE id = $id";
+    nama = '$nama',
+    nrp = '$nrp',
+    email = '$email',
+    jurusan = '$jurusan',
+    gambar = '$gambar'
+  WHERE id = $id";
   mysqli_query($conn, $query) or die(mysqli_error($conn));
   return mysqli_affected_rows($conn);
 }
@@ -77,11 +153,10 @@ function cari($keyword)
 {
   $conn = koneksi();
 
-  $query = "SELECT * FROM mahasiswa
-              WHERE 
-            nama LIKE '%$keyword%' OR
-            nrp LIKE '%$keyword%'
-          ";
+  $query = "SELECT * FROM mahasiswa WHERE 
+    nama LIKE '%$keyword%' OR
+    nrp LIKE '%$keyword%'
+  ";
 
   $result = mysqli_query($conn, $query);
 
@@ -128,37 +203,49 @@ function registrasi($data)
 
   // jika username / password kosong
   if (empty($username) || empty($password2) || empty($password2)) {
-    echo "<script>
-            alert('username / password tidak boleh kosong!');
-            document.location.href = 'registrasi.php';
-          </script>";
+    echo
+    "
+      <script>
+        alert('username / password tidak boleh kosong!');
+        document.location.href = 'registrasi.php';
+      </script>
+    ";
     return false;
   }
 
   // jika username sudah ada 
   if (query("SELECT * FROM user WHERE username = '$username'")) {
-    echo "<script>
-            alert('username sudah terdaftar!');
-            document.location.href = 'registrasi.php';
-          </script>";
+    echo
+    "
+      <script>
+        alert('username sudah terdaftar!');
+        document.location.href = 'registrasi.php';
+      </script>
+    ";
     return false;
   }
 
   // jika konfirmasi password tidak sesuai
   if ($password1 !== $password2) {
-    echo "<script>
-            alert('konfirmasi password tidak sesuai!');
-            document.location.href = 'registrasi.php';
-          </script>";
+    echo
+    "
+      <script>
+        alert('konfirmasi password tidak sesuai!');
+        document.location.href = 'registrasi.php';
+      </script>
+    ";
     return false;
   }
 
   // jika password < 5 digit
   if (strlen($password1) < 5) {
-    echo "<script>
-            alert('password terlalu pendek!');
-            document.location.href = 'registrasi.php';
-          </script>";
+    echo
+    "
+      <script>
+        alert('password terlalu pendek!');
+        document.location.href = 'registrasi.php';
+      </script>
+    ";
     return false;
   }
 
@@ -166,10 +253,11 @@ function registrasi($data)
   // enkripsi password
   $password_baru = password_hash($password1, PASSWORD_DEFAULT);
   // insert ke tabel user
-  $query = "INSERT INTO user
-              VALUES
-            (null, '$username', '$password_baru')
-          ";
+  $query = "INSERT INTO user VALUES(
+    null, 
+    '$username', 
+    '$password_baru'
+  )";
   mysqli_query($conn, $query) or die(mysqli_error($conn));
   return mysqli_affected_rows($conn);
 }
